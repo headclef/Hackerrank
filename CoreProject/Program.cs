34af6ca37a3c3ebc237fc832377c4a05eb2bd521
FURKAN TURAL <furkanturalofficial@outlook.com>
2025-10-29 14:44:25 +0300

    There are few ways to solve the problem. I'll just be doing approach one in this commit.
    version 0.0.2 - alpha

--- CoreProject/Program.cs
+++ CoreProject/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Algorithm
+ * a = 7
+ * b = 3
+ * 
+ * static void Main(String[] args) {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1,val2);
+        Console.WriteLine(sum);
+    }
+ * 
+ * Define a solveMeFirst method that sums the value of two different integers
+ */
+
+// There are few ways to solve the problem.
+// I'll just do the version that comes to my mind.
+
+// Flow
+static async Task Main()
+{
+    int a = 7;
+    int b = 3;
+    int sum = 0;
+    sum = await SolveMeFirst(a, b, Approach.First);
+}
+
+// First approach
+static async Task<int> SolveMeFirst(int a, int b, Approach approach)
+{
+    switch (approach)
+    {
+        case Approach.First:
+            return await ApproachOne(a, b);
+        default:
+            return await Task.FromResult(0);
+    }
+}
+
+static async Task<int> ApproachOne(int a, int b)
+{
+    return await Task.FromResult(a + b);
+}
+
+enum Approach
+{
+    First
+}
