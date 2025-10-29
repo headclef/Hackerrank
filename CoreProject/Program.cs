981ef427655478b96f43c9c01a16e256eb844d56
FURKAN TURAL <furkanturalofficial@outlook.com>
2025-10-29 14:47:53 +0300

    Another approach included.
    version 0.0.3 - alpha.

--- CoreProject/Program.cs
+++ CoreProject/Program.cs
@@ -22,26 +22,46 @@ static async Task Main()
     int b = 3;
     int sum = 0;
     sum = await SolveMeFirst(a, b, Approach.First);
+    sum = await SolveMeFirst(a, b, Approach.Second);
 }
 
-// First approach
+/// <summary>
+/// Pick which one you'll choose and return the answer
+/// </summary>
 static async Task<int> SolveMeFirst(int a, int b, Approach approach)
 {
     switch (approach)
     {
         case Approach.First:
             return await ApproachOne(a, b);
+        case Approach.Second:
+            return await Approachtwo(a, b);
         default:
             return await Task.FromResult(0);
     }
 }
 
+/// <summary>
+/// First approach
+/// </summary>
 static async Task<int> ApproachOne(int a, int b)
 {
     return await Task.FromResult(a + b);
 }
 
+/// <summary>
+/// Second approach
+/// </summary>
+static async Task<int> Approachtwo(int a, int b)
+{
+    int response = 0;
+    response += a;
+    response += b;
+    return await Task.FromResult(response);
+}
+
 enum Approach
 {
-    First
+    First,
+    Second
 }
